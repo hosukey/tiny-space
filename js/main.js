21719f05fa250601b6d37365e9f7924199dbc12a
hosu <hosulee11@gmail.com>
2022-02-09 15:47:59 -0800

    feat: add items to the cart using local storage

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 'use strict';
-// filter items
+// Filter Items -------------------------------------------
 const filterTitle = document.querySelectorAll('.filter-title');
 const allCategoryPosts = document.querySelectorAll('.all');
 
@@ -28,7 +28,8 @@ function changeActivePosition(activeItem) {
   activeItem.classList.add('active');
 }
 
-// cart
+// Cart ---------------------------------------------------
+// Cart Open/Close
 const cartNavi = document.querySelector('.navi__right-cart');
 const cart = document.querySelector('.addcart');
 const cartCloseButton = document.querySelector('.cart-close');
@@ -43,3 +44,167 @@ function displayCart() {
 function closeCart() {
   cart.style.transform = 'translateX(100%)';
 }
+
+// Cart Add/Remove Items
+const addToCartBtns = document.querySelectorAll('.cart');
+
+// Products List
+let products = [
+  {
+    name: 'Dolphine Accent Chair',
+    tag: 'dolphine',
+    price: 300,
+    inCart: 0,
+  },
+  {
+    name: 'Karina Accent Chair',
+    tag: 'karina',
+    price: 199,
+    inCart: 0,
+  },
+  {
+    name: 'Sydney Stool',
+    tag: 'sydney',
+    price: 199,
+    inCart: 0,
+  },
+  {
+    name: 'Elly Stool',
+    tag: 'elly',
+    price: 150,
+    inCart: 0,
+  },
+  {
+    name: 'Amy Accent Chair',
+    tag: 'amy',
+    price: 400,
+    inCart: 0,
+  },
+  {
+    name: 'Novo Fabric Sofa',
+    tag: 'novo',
+    price: 1200,
+    inCart: 0,
+  },
+  {
+    name: 'Kayden bench',
+    tag: 'kayden',
+    price: 300,
+    inCart: 0,
+  },
+  {
+    name: 'Sofia leather sofa',
+    tag: 'sofia',
+    price: 1800,
+    inCart: 0,
+  },
+  {
+    name: 'Calla suede sofa',
+    tag: 'calla',
+    price: 1299,
+    inCart: 0,
+  },
+];
+
+// Click event listener on Add to Cart Button
+for (let i = 0; i < addToCartBtns.length; i++) {
+  addToCartBtns[i].addEventListener('click', () => {
+    cartNumbers(products[i]);
+    totalCost(products[i]);
+  });
+}
+
+// Cart Numbers updated whenever item exist in cart
+function onLoadCartNumbers() {
+  let productNumbers = localStorage.getItem('cartNumbers');
+  if (productNumbers) {
+    document.querySelector('.cartBtn span').textContent = productNumbers;
+    document.querySelector('.cart-added span').textContent = productNumbers;
+  }
+}
+
+function cartNumbers(product) {
+  //   console.log('the product clicked is', product);
+  let productNumbers = localStorage.getItem('cartNumbers');
+  productNumbers = parseInt(productNumbers);
+  if (productNumbers) {
+    localStorage.setItem('cartNumbers', productNumbers + 1);
+    document.querySelector('.cartBtn span').textContent = productNumbers + 1;
+    document.querySelector('.cart-added span').textContent = productNumbers + 1;
+  } else {
+    localStorage.setItem('cartNumbers', 1);
+    document.querySelector('.cartBtn span').textContent = 1;
+  }
+
+  setItems(product);
+}
+
+function setItems(product) {
+  let cartItems = localStorage.getItem('productsInCart');
+  cartItems = JSON.parse(cartItems);
+
+  if (cartItems !== null) {
+    if (cartItems[product.tag] == undefined) {
+      cartItems = {
+        ...cartItems,
+        [product.tag]: product,
+      };
+    }
+    cartItems[product.tag].inCart += 1;
+  } else {
+    product.inCart = 1;
+    cartItems = {
+      [product.tag]: product,
+    };
+  }
+
+  localStorage.setItem('productsInCart', JSON.stringify(cartItems));
+}
+
+// adding up total cost
+function totalCost(product) {
+  //   console.log('The product price is', product.price);
+  let cartCost = localStorage.getItem('totalCost');
+
+  if (cartCost != null) {
+    cartCost = parseInt(cartCost);
+    localStorage.setItem('totalCost', cartCost + product.price);
+  } else {
+    localStorage.setItem('totalCost', product.price);
+  }
+}
+
+function displayCartItems() {
+  let cartItems = localStorage.getItem('productsInCart');
+  let cartItemsContainer = document.querySelector('.cart-items');
+  let cartCost = localStorage.getItem('totalCost');
+  cartItems = JSON.parse(cartItems);
+  if (cartItems) {
+    cartItemsContainer.innerHTML = '';
+    Object.values(cartItems).map((item) => {
+      cartItemsContainer.innerHTML += `
+<div class="cart-items__item">
+<div class="image">
+  <img src="img/webp/${item.tag}.webp" alt="" />
+  <div class="name">${item.name}</div>
+</div>
+<div class="amount">
+  <div class="amount-top">
+    <div class="count">x${item.inCart}</div>
+    <div class="price">$${item.inCart * item.price}</div>
+  </div>
+  <div class="remove"><button>remove</button></div>
+</div>
+</div>
+`;
+
+      document.querySelector('.cart-totals').innerHTML = `
+Subtotal $${cartCost}
+`;
+    });
+  }
+  console.log(cartItems);
+}
+
+onLoadCartNumbers();
+displayCartItems();
